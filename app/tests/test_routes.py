8e9b50c470ecffb59361fc2593e46348b1923dfc
Mona Lin <monalin2002@gmail.com>
2022-10-31 23:51:17 -0400

    Changed parameter values to reflect updated API yes/no to  boolean status

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import pytest
 from app.handlers.routes import configure_routes
 
 
@@ -10,6 +10,122 @@ def test_base_route():
     url = '/'
 
     response = client.get(url)
-
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
+
+def test_predict_allFields():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    data = {
+        "sex": "F",
+        "age" : 18,
+        "address": "U",
+        "famsize": "GT3",
+        "Pstatus": "A",
+        "Medu": 4,
+        "Fedu": 4,
+        "Mjob:": "at_home",
+        "Fjob": "teacher",
+        "reason" : "course",
+        "guardian": "mother",
+        "traveltime": 2,
+        "studytime": 2,
+        "failures": 0,
+        "schoolsup": True,
+        "famsup": False,
+        "paid": False,
+        "activities": False,
+        "nursery": True,
+        "higher": True,
+        "internet": False,
+        "romantic": False,
+        "famrel": 4,
+        "freetime": 3,
+        "goout": 4,
+        "Dalc": 1,
+        "Wcal": 1,
+        "health": 3,
+        "absences": 6
+    }
+    response = client.get('/predict', json = data)
+    output = response.get_data()
+    assert response.status_code == 200
+    assert output == data
+    assert response.headers.get('Content-Type') == 'application/json'
+
+def test_predict_age_missing():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    data = {
+        "age" : 18,
+        "address": "U",
+        "famsize": "GT3",
+        "Pstatus": "A",
+        "Medu": 4,
+        "Fedu": 4,
+        "Mjob:": "at_home",
+        "Fjob": "teacher",
+        "reason" : "course",
+        "guardian": "mother",
+        "traveltime": 2,
+        "studytime": 2,
+        "failures": 0,
+        "schoolsup": True,
+        "famsup": False,
+        "paid": False,
+        "activities": False,
+        "nursery": True,
+        "higher": True,
+        "internet": False,
+        "romantic": False,
+        "famrel": 4,
+        "freetime": 3,
+        "goout": 4,
+        "Dalc": 1,
+        "Wcal": 1,
+        "health": 3,
+        "absences": 6
+    }
+    response = client.get('/predict', json = data)
+    output = response.get_data()
+    assert response.status_code == 400
+
+def test_predict_age_missing():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    data = {
+        "sex": "F",
+        "address": "U",
+        "famsize": "GT3",
+        "Pstatus": "A",
+        "Medu": 4,
+        "Fedu": 4,
+        "Mjob:": "at_home",
+        "Fjob": "teacher",
+        "reason" : "course",
+        "guardian": "mother",
+        "traveltime": 2,
+        "studytime": 2,
+        "failures": 0,
+        "schoolsup": True,
+        "famsup": False,
+        "paid": False,
+        "activities": False,
+        "nursery": True,
+        "higher": True,
+        "internet": False,
+        "romantic": False,
+        "famrel": 4,
+        "freetime": 3,
+        "goout": 4,
+        "Dalc": 1,
+        "Wcal": 1,
+        "health": 3,
+        "absences": 6
+    }
+    response = client.get('/predict', json = data)
+    output = response.get_data()
+    assert response.status_code == 400
