93c123e84a30a27b463659640b72cf048504eb69
Mona Lin <monalin2002@gmail.com>
2022-10-31 23:53:57 -0400

    Added more tests

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -54,7 +54,7 @@ def test_predict_allFields():
     assert output == data
     assert response.headers.get('Content-Type') == 'application/json'
 
-def test_predict_age_missing():
+def test_predict_sex_missing():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
@@ -98,6 +98,43 @@ def test_predict_age_missing():
     client = app.test_client()
     data = {
         "sex": "F",
+        "address": "U",
+        "famsize": "GT3",
+        "Pstatus": "A",
+        "Medu": 4,
+        "Fedu": 4,
+        "Mjob:": "at_home",
+        "Fjob": "teacher",
+        "reason" : "course",
+        "guardian": "mother",
+        "traveltime": 2,
+        "studytime": 2,
+        "failures": 0,
+        "schoolsup": True,
+        "famsup": False,
+        "paid": False,
+        "activities": False,
+        "nursery": True,
+        "higher": True,
+        "internet": False,
+        "romantic": False,
+        "famrel": 4,
+        "freetime": 3,
+        "goout": 4,
+        "Dalc": 1,
+        "Wcal": 1,
+        "health": 3,
+        "absences": 6
+    }
+    response = client.get('/predict', json = data)
+    output = response.get_data()
+    assert response.status_code == 400
+
+def test_predict_sex_and_age_missing():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    data = {
         "address": "U",
         "famsize": "GT3",
         "Pstatus": "A",
