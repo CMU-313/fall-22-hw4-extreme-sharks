8f28c8da939bcf677cc5b1034c752fb413109771
Mona Lin <monalin2002@gmail.com>
2022-11-01 20:35:46 -0400

    Added unit tests with incorrect values

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -130,6 +130,84 @@ def test_predict_age_missing():
     output = response.get_data()
     assert response.status_code == 400
 
+def test_predict_sex_invalid():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    data = {
+        "sex": "A",
+        "age" : 18,
+        "address": "U",
+        "famsize": "GT3",
+        "Pstatus": "A",
+        "Medu": 4,
+        "Fedu": 4,
+        "Mjob:": "at_home",
+        "Fjob": "teacher",
+        "reason" : "course",
+        "guardian": "mother",
+        "traveltime": 2,
+        "studytime": 2,
+        "failures": 0,
+        "schoolsup": True,
+        "famsup": False,
+        "paid": False,
+        "activities": False,
+        "nursery": True,
+        "higher": True,
+        "internet": False,
+        "romantic": False,
+        "famrel": 4,
+        "freetime": 3,
+        "goout": 4,
+        "Dalc": 1,
+        "Wcal": 1,
+        "health": 3,
+        "absences": 6
+    }
+    response = client.get('/predict', json = data)
+    output = response.get_data()
+    assert response.status_code == 400
+
+def test_predict_age_invalid():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    data = {
+        "sex": "F",
+        "age" : 10000,
+        "address": "U",
+        "famsize": "GT3",
+        "Pstatus": "A",
+        "Medu": 4,
+        "Fedu": 4,
+        "Mjob:": "at_home",
+        "Fjob": "teacher",
+        "reason" : "course",
+        "guardian": "mother",
+        "traveltime": 2,
+        "studytime": 2,
+        "failures": 0,
+        "schoolsup": True,
+        "famsup": False,
+        "paid": False,
+        "activities": False,
+        "nursery": True,
+        "higher": True,
+        "internet": False,
+        "romantic": False,
+        "famrel": 4,
+        "freetime": 3,
+        "goout": 4,
+        "Dalc": 1,
+        "Wcal": 1,
+        "health": 3,
+        "absences": 6
+    }
+    response = client.get('/predict', json = data)
+    output = response.get_data()
+    assert response.status_code == 400
+
 def test_predict_sex_and_age_missing():
     app = Flask(__name__)
     configure_routes(app)
@@ -166,3 +244,4 @@ def test_predict_sex_and_age_missing():
     response = client.get('/predict', json = data)
     output = response.get_data()
     assert response.status_code == 400
+
