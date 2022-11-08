40d2f35584c1703086d64b5557e5a46c3fb37c7d
Nicolas Ettlin <nicolas.ettlin@me.com>
2022-11-07 22:55:47 -0500

    Make the API run the model

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -1,52 +1,45 @@
 from app.handlers.validation import *
-from flask import Flask, jsonify, request
+from flask import jsonify, request
 import joblib
 import pandas as pd
-import numpy as np
 import os
 
 
 def configure_routes(app):
     this_dir = os.path.dirname(__file__)
     model_path = os.path.join(this_dir, "model.pkl")
-    # clf = joblib.load(model_path)
+    clf = joblib.load(model_path)
 
     schema = [
-        BinaryParameter('sex', 'F', 'M'),
-        NumericParameter('age', 15, 22),
-
-        BinaryParameter('address', 'U', 'R'),
-        BinaryParameter('famsize', 'LE3', 'GT3'),
-        BinaryParameter('Pstatus', 'T', 'A'),
-
-        NumericParameter('Medu', 0, 4),
+        NumericParameter('Dalc', 1, 5),
         NumericParameter('Fedu', 0, 4),
-
-        NominalParameter('Mjob', ['teacher', 'health', 'services', 'at_home', 'other']),
         NominalParameter('Fjob', ['teacher', 'health', 'services', 'at_home', 'other']),
-        NominalParameter('reason', ['home', 'reputation', 'course', 'other']),
-        NominalParameter('guardian', ['mother', 'father', 'other']),
-
-        NumericParameter('traveltime', 1, 4),
-        NumericParameter('studytime', 1, 4),
-        NumericParameter('failures', 1, 4),
-
-        BinaryParameter('schoolsup', 'true', 'false'),
-        BinaryParameter('famsup', 'true', 'false'),
-        BinaryParameter('paid', 'true', 'false'),
+        NumericParameter('Medu', 0, 4),
+        NominalParameter('Mjob', ['teacher', 'health', 'services', 'at_home', 'other']),
+        BinaryParameter('Pstatus', 'T', 'A'),
+        NumericParameter('Walc', 1, 5),
+        NumericParameter('absences', 0, 93),
         BinaryParameter('activities', 'true', 'false'),
-        BinaryParameter('nursery', 'true', 'false'),
-        BinaryParameter('higher', 'true', 'false'),
-        BinaryParameter('internet', 'true', 'false'),
-        BinaryParameter('romantic', 'true', 'false'),
-
+        BinaryParameter('address', 'U', 'R'),
+        NumericParameter('age', 15, 22),
+        NumericParameter('failures', 1, 4),
         NumericParameter('famrel', 1, 5),
+        BinaryParameter('famsize', 'LE3', 'GT3'),
+        BinaryParameter('famsup', 'true', 'false'),
         NumericParameter('freetime', 1, 5),
         NumericParameter('goout', 1, 5),
-        NumericParameter('Dalc', 1, 5),
-        NumericParameter('Walc', 1, 5),
+        NominalParameter('guardian', ['mother', 'father', 'other']),
         NumericParameter('health', 1, 5),
-        NumericParameter('absences', 0, 93),
+        BinaryParameter('higher', 'true', 'false'),
+        BinaryParameter('internet', 'true', 'false'),
+        BinaryParameter('nursery', 'true', 'false'),
+        BinaryParameter('paid', 'true', 'false'),
+        NominalParameter('reason', ['home', 'reputation', 'course', 'other']),
+        BinaryParameter('romantic', 'true', 'false'),
+        BinaryParameter('schoolsup', 'true', 'false'),
+        BinaryParameter('sex', 'F', 'M'),
+        NumericParameter('studytime', 1, 4),
+        NumericParameter('traveltime', 1, 4),
     ]
 
     @app.route('/')
@@ -61,21 +54,13 @@ def configure_routes(app):
         except ValidationError as validation_error:
             return jsonify({'error': validation_error.message}), 400
 
-        return jsonify(model_input)
+        # Wrap values in pandas series
+        model_input_series = {}
+        for parameter in model_input:
+            model_input_series[parameter] = pd.Series(model_input[parameter])
 
-        # use entries from the query string here but could also use json
-        age = request.args.get('age')
-        absences = request.args.get('absences')
-        health = request.args.get('health')
-        data = [[age], [health], [absences]]
+        # Run the model
+        query_df = pd.DataFrame(model_input_series)
+        prediction = clf.predict(query_df)[0]
 
-        query_df = pd.DataFrame({
-            'age': pd.Series(age),
-            'health': pd.Series(health),
-            'absences': pd.Series(absences)
-        })
-
-        # prediction = clf.predict(query)
-        # prediction = 1
-        # return jsonify(np.ndarray.item(prediction))
-        return jsonify({'isQualified': True})
+        return jsonify({'isQualified': prediction == 1})
